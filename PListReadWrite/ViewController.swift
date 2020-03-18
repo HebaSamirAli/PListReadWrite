09f3fea54170c1965a0720e790d554ded4ec6cfa
Heba Ali <hshegy2002@gmail.com>
2020-03-18 21:26:50 +0300

    Initial Commit

---
+++ PListReadWrite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PListReadWrite
+//
+//  Created by HebaMac on 3/18/20.
+//  Copyright © 2020 HSAAA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
